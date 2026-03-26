ef48f552a67242adb41e7c8bb26c3bb20b227502
Abderrazek Chamekh <Abderrazek.Chamekh@esprit.tn>
2026-03-26 15:12:39 +0100

    Add Docker setup: Dockerfiles, dockerignore, docker-compose

--- Back_End/config/connect.js
+++ Back_End/config/connect.js
@@ -1,7 +1,6 @@
 const mongoose =require('mongoose')
 
-mongoose.connect('mongodb://127.0.0.1:27017/Telnet')
-.then(
+mongoose.connect(process.env.MONGO_URI).then(
     ()=>{
         console.log('Connected to BD')
     }
